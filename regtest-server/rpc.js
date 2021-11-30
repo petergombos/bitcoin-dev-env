9558f5e09e30e4f60be86dc09307412b46beeafd
Peter Gombos <pepegombos@gmail.com>
2021-11-30 17:53:16 +0100

    Update dev env to use electrs + esplora (blockstream).

--- regtest-server/rpc.js
+++
@@ -1,5 +0,0 @@
-module.exports = require("yajrpc/qup")({
-  url: process.env.RPC || "http://localhost:8332",
-  batch: process.env.RPCBATCHSIZE || 500,
-  concurrent: process.env.RPCCONCURRENT || 16,
-});
--- src/index.js
+++ src/index.js
@@ -16,27 +16,24 @@ const p2wpkhAlice1 = bitcoin.payments.p2wpkh({
 async function main() {
   const psbt = new bitcoin.Psbt({ network });
   await psbt.addInput({
-    hash: "7b52989bc4f3d7590f2177689d68de877f69f6d566e0b6ae640296137b8669f4",
+    hash: "b27a1ca7df882c88144cafa482163b1e587ab2f31805cc419b084361e5fcd8bd",
     index: 0,
     witnessUtxo: {
       script: Buffer.from("0014" + alice[1].pubKeyHash, "hex"),
-      value: 125e7,
+      value: 50e8,
     },
   });
   await psbt.addOutput({
     address: bob[1].p2pkh,
-    value: 5e7 - 1000,
+    value: 50e8 - 1000,
   });
-  await psbt.addOutput({
-    address: alice[1].p2wpkh,
-    value: 12e8 - 1000,
-  });
-
+  // await psbt.addOutput({
+  //   address: alice[1].p2wpkh,
+  //   value: 12e8 - 1000,
+  // });
   psbt.signInput(0, keyPairAlice1);
   psbt.validateSignaturesOfInput(0);
-
   psbt.finalizeAllInputs();
-
   console.log("Transaction hexadecimal:");
   const tx = psbt.extractTransaction().toHex();
   console.log(tx);
